7d046af054c4180d7ceef7420a6e71bde3098312
lamsond <dlamson13@gmail.com>
2020-01-15 16:19:43 -0500

    switched from thread timing to swing timer

--- src/org/whrhs/dlamson/java/csa/scy/MainPanel.java
+++ src/org/whrhs/dlamson/java/csa/scy/MainPanel.java
@@ -3,22 +3,26 @@ package org.whrhs.dlamson.java.csa.scy;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class MainPanel extends JPanel implements Runnable{
+public class MainPanel extends JPanel implements ActionListener{
 
 	public static final int NUM_BOXES = 50;
 	public static final int W = 1001, H = 700;
-	public static final int FPS = 20;
+	public static final int DELAY = 20;
 	private Color randColor;
 	private Color boxColor;
 	private Box[] boxes;
-	private Thread animation;
+	private Timer refreshTimer;
 	private int xOff;
 	
 	public MainPanel() {
 		boxes = new Box[NUM_BOXES];
 		randColor = new Color((float) Math.random(), (float) Math.random(), (float) Math.random());
+		System.out.println(randColor);
 		boxColor = randColor.darker();
 		setBackground(randColor);
 		setPreferredSize(new Dimension(W, H));
@@ -26,6 +30,15 @@ public class MainPanel extends JPanel implements Runnable{
 		for(int i = 0; i < boxes.length; i++) {
 			boxes[i] = new Box(boxColor);	
 		}
+		
+		refreshTimer = new Timer(DELAY, this);
+		refreshTimer.start();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		move();
+		repaint();
 	}
 	
 	public void move() {
@@ -35,45 +48,10 @@ public class MainPanel extends JPanel implements Runnable{
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		for(int i = 0; i < boxes.length; i++){
+		g.drawOval(xOff, H/2, 10, 10);
+		/*for(int i = 0; i < boxes.length; i++){
 			boxes[i].drawBox(g, i*W/NUM_BOXES + xOff);
-		}
+		}*/
 	}
-	
-	@Override
-	public void addNotify() {
-		super.addNotify();
-		animation = new Thread(this);
-		animation.start();	
-	}
-	
-	@Override
-	public void run() {
-		long startTime, endTime, dt, sleepTime;
-		
-		while(true) {
-			startTime = System.currentTimeMillis();
-			
-			move();
-			repaint();
-			
-			endTime = System.currentTimeMillis();
-			dt = endTime - startTime;
-			System.out.println(dt);
-			sleepTime = Math.max(FPS - dt, 2);
-			
-			try {
-				Thread.sleep(sleepTime);
-			}
-			catch(InterruptedException e) {
-				System.out.println(e);
-			}
-			
-			
-		}
-		
-		
-	}
-	
 	
 }
